75633994fd71e89c718b1ab875b860e42776a175
Rodion Ibragimov <rodionibragimov@MBP-Rodion.Dlink>
2020-10-30 02:39:31 +0300

    Добавил возможность подключать разные прокси, карявые проверки

--- parserNews.py
+++ parserNews.py
@@ -2,64 +2,87 @@ from bs4 import BeautifulSoup
 import requests
 from fake_useragent import UserAgent
 import time
+from datetime import datetime
 import json
+from requests.auth import HTTPProxyAuth
+from storage import *
+from requests import exceptions
 
-res = []
-linkTinkoff = "https://newssearch.yandex.ru/yandsearch?text=%D1%82%D0%B8%D0%BD%D1%8C%D0%BA%D0%BE%D1%84%D1%84+%D0%B1%D0%B0%D0%BD%D0%BA&rpt=nnews2&rel=tm"
-linkVtb = "https://newssearch.yandex.ru/yandsearch?text=%D0%B2%D1%82%D0%B1+%D0%B1%D0%B0%D0%BD%D0%BA&rpt=nnews2&rel=tm"
-bankNames = ['Tinkoff', 'Vtb']
-bankLinks = [linkTinkoff, linkVtb]
-with open("res.json", "r") as readFile:
-    data = json.load(readFile)
-# proxies = {'http': 'http://45.169.163.217:8080',
-#         'https': 'http://200.69.84.125:8080'}
+
+# auth = HTTPProxyAuth('Cfamoscow','8u43jl')
 # user_agent = ('Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:50.0) '
 #             'Gecko/20100101 Firefox/50.0')
 
 
-def getHtml(bankLink):
-    req = requests.get(bankLink, headers={'User-Agent': UserAgent().chrome})
-    # req = requests.get(linkTinkoff)
-    return req
+def getHtml(bankLink, proxies):
+    req = requests.get(bankLink, headers={'User-Agent': UserAgent().chrome}, proxies=proxies)
+    # print(req.status_code)
+    return req 
+    
 
 
-def getData(html, counter):
+def getData(html, counterBankName):
+    with open("res.json", "r") as readFile:
+        data = json.load(readFile)
     repeatCheck = False
     soup = BeautifulSoup(html.content, 'html.parser')
-    print(soup.prettify())
+    # print(soup.prettify())
     time.sleep(5)
     allNews = soup.findAll('div', {'class': 'document__title'})
-    for news in allNews:
-        time.sleep(6)
-        link = news.find('a').get('href')
-        bank = {
-            'bankName': bankNames[counter],
-            'link': link
-        } 
-        for a in range(len(data)):
-            if bank['link'] == data[a]['link']:
-                print("Error, repeating news: ", bank)
-                repeatCheck = True
-        if repeatCheck == False:
-            res.append(bank)
+    if allNews != []:
+        for news in allNews:
+            time.sleep(6)
+            link = news.find('a').get('href')
+            bank = {
+                'bankName': bankNames[counterBankName],
+                'link': link,
+                'timestamp': datetime.now().strftime("%d-%m-%Y %H:%M")
+            } 
+            for a in range(len(data)):
+                if bank['link'] == data[a]['link']:
+                    print("Error, repeating news: ", bank)
+                    repeatCheck = True
+            if repeatCheck == False:
+                res.append(bank)
+            else:
+                repeatCheck = False
+            with open('res.json', 'w') as file:
+                json.dump(res, file, indent=2, ensure_ascii=False)
         else:
-            repeatCheck = False
-        with open('res.json', 'w') as file:
-            json.dump(res, file, indent=2, ensure_ascii=False)
+            print('Error, yandex banned you')
+            
+            
+
+            
 
 
 def main():
+    z = 0
     while True:
         for i in range(0, len(bankLinks)):
-            html = getHtml(bankLinks[i])
-            getData(html, i)
+            if z >= len(allProxies):
+                z = 0
+            try:
+                html = getHtml(bankLinks[i], allProxies[z])
+                soup = BeautifulSoup(html.content, 'html.parser')
+                allNews = soup.findAll('div', {'class': 'document__title'})
+                
+                if allNews != []:
+                    getData(html, i)
+                else:
+                    print('Error, yandex banned you, trying next proxies...', allProxies[z])
+                    z += 1
+            except requests.exceptions.ProxyError:
+                print('Bad proxy connection, changing proxy...', allProxies[z])
+                z += 1 
+            print(z)
             time.sleep(20)
 
     
 
 
 if __name__ == '__main__':
-        main()
+    main()
 
 
 # print(soup.prettify())
