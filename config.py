bf37c4cbdfbff92c4043228f7a5c6b3910282b3e
Rodion Ibragimov <rodionibragimov@MBP-Rodion.Dlink>
2020-11-19 20:43:33 +0300

    Config

---
+++ config.py
@@ -0,0 +1,33 @@
+linkPlus = 'https://newssearch.yandex.ru/yandsearch?text=%D0%9F%D0%90%D0%9E+%22%D0%9F%D0%BB%D1%8E%D1%81+%D0%91%D0%B0%D0%BD%D0%BA%22&rpt=nnews2&rel=tm'
+linkSdm = 'https://newssearch.yandex.ru/yandsearch?text=%D0%A1%D0%94%D0%9C-%D0%91%D0%B0%D0%BD%D0%BA+(%D0%9F%D0%90%D0%9E)&rpt=nnews2&rel=tm'
+linkAsian = 'https://newssearch.yandex.ru/yandsearch?text=%22%D0%90%D0%B7%D0%B8%D0%B0%D1%82%D1%81%D0%BA%D0%BE-%D0%A2%D0%B8%D1%85%D0%BE%D0%BE%D0%BA%D0%B5%D0%B0%D0%BD%D1%81%D0%BA%D0%B8%D0%B9+%D0%91%D0%B0%D0%BD%D0%BA%22+(%D0%9F%D0%90%D0%9E)&rpt=nnews2&rel=tm'
+linkRusStandart = 'https://newssearch.yandex.ru/yandsearch?text=%D0%90%D0%9E+%22%D0%91%D0%B0%D0%BD%D0%BA+%D0%A0%D1%83%D1%81%D1%81%D0%BA%D0%B8%D0%B9+%D0%A1%D1%82%D0%B0%D0%BD%D0%B4%D0%B0%D1%80%D1%82%22&rpt=nnews2&rel=tm'
+linkPoidem = 'https://newssearch.yandex.ru/yandsearch?text=%D0%90%D0%9E+%D0%9A%D0%91+%22%D0%9F%D0%BE%D0%B9%D0%B4%D1%91%D0%BC!%22&rpt=nnews2&rel=tm'
+linkTinkoff = 'https://newssearch.yandex.ru/yandsearch?text=%D0%90%D0%9E+%22%D0%A2%D0%B8%D0%BD%D1%8C%D0%BA%D0%BE%D1%84%D1%84+%D0%91%D0%B0%D0%BD%D0%BA%22&rpt=nnews2&rel=tm'
+linkExpo = 'https://newssearch.yandex.ru/yandsearch?text=%D0%9E%D0%9E%D0%9E+%22%D0%AD%D0%BA%D1%81%D0%BF%D0%BE%D0%B1%D0%B0%D0%BD%D0%BA%22&rpt=nnews2&rel=tm'
+linkCredit = 'https://newssearch.yandex.ru/yandsearch?text=%D0%90%D0%9E+%22%D0%9A%D1%80%D0%B5%D0%B4%D0%B8%D1%82+%D0%95%D0%B2%D1%80%D0%BE%D0%BF%D0%B0+%D0%91%D0%B0%D0%BD%D0%BA%22&rpt=nnews2&rel=tm'
+linkMercedes = 'https://newssearch.yandex.ru/yandsearch?text=%22%D0%9C%D0%B5%D1%80%D1%81%D0%B5%D0%B4%D0%B5%D1%81-%D0%91%D0%B5%D0%BD%D1%86+%D0%91%D0%B0%D0%BD%D0%BA+%D0%A0%D1%83%D1%81%22+%D0%9E%D0%9E%D0%9E&rpt=nnews2&rel=tm'
+
+res = []
+
+
+bankNames = ['ПАО "Плюс Банк"', 'СДМ-Банк (ПАО)', '"Азиатско-Тихоокеанский Банк" (ПАО)', 'АО "Банк Русский Стандарт"', 'АО КБ "Пойдём!"', 'АО "Тинькофф Банк"', 'ООО "Экспобанк"', 'АО "Кредит Европа Банк"', '"Мерседес-Бенц Банк Рус" ООО']
+
+bankLinks = [linkPlus, linkSdm, linkAsian, linkRusStandart, linkPoidem, linkTinkoff, linkExpo, linkCredit, linkMercedes]
+
+
+proxies1 = {'https': 'http://Baupwu:pD0rl9nJ4V@45.89.19.60:11100'}
+proxies2 = {'https': 'http://Baupwu:pD0rl9nJ4V@45.89.18.241:11744'}
+proxies3 = {'https': 'http://rYvfHywqUz:T7mJ0QtwVR@213.139.229.127:45882'}
+proxies4 = {'https': 'http://Baupwu:pD0rl9nJ4V@45.89.19.108:19076'}
+proxies5 = {'https': 'http://Baupwu:pD0rl9nJ4V@45.89.19.6:13264'}
+proxies6 = {'https': 'http://Baupwu:pD0rl9nJ4V@45.89.19.2:6246'}
+
+# allProxies = [proxies1, False, proxies3, proxies2, proxies4, proxies5, proxies6]
+allProxies = [False]
+token = '1412870284:AAF-YWMf4tTEsEUbn_enU4cD_yoWpbEUOpU'
+apiId = 1941734
+apiHash = 'c43754f41c681882da1dc430d3d0e59f'
+sessionName = 'yo'
+
+# print(len(bankLinks))
